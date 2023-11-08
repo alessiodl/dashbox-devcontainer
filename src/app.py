e1dbd75170817f6bff49dd03a3fa166dedea5559
alessio <alessio.dl@gmail.com>
2023-11-08 12:04:31 +0100

    aggiornato requirements

---
+++ src/app.py
@@ -0,0 +1,24 @@
+from dash import dash, Dash, html, dcc, callback, Output, Input
+import plotly.express as px
+import pandas as pd
+
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
+
+app = Dash()
+
+app.layout = html.Div([
+    html.H1(children='Boilerplate app on Dash v. {}'.format(dash.__version__), style={'textAlign':'center'}),
+    dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
+    dcc.Graph(id='graph-content')
+])
+
+@callback(
+    Output('graph-content', 'figure'),
+    Input('dropdown-selection', 'value')
+)
+def update_graph(value):
+    dff = df[df.country==value]
+    return px.line(dff, x='year', y='pop')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
--- src/main.py
+++
@@ -1,5 +0,0 @@
-import earthaccess
-import pandas as pd
-
-print('NASA Earthaccess v. {0}'.format(earthaccess.__version__))
-print('Pandas: {0} v'.format(pd.__version__))
