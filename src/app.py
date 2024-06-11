8d40c1662384de5ae0aa2349831a6ffbe7bc1e18
Alessio Di Lorenzo <a.dilorenzo@izs.intra>
2024-06-11 17:08:54 +0200

    Dash mantine components added

--- src/app.py
+++ src/app.py
@@ -1,9 +1,12 @@
-from dash import dash, Dash, html, dcc, callback, Output, Input
+from dash import dash, Dash, html, dcc, _dash_renderer, callback, Output, Input
 import dash_bootstrap_components as dbc
+import dash_mantine_components as dmc
 import dash_leaflet as dl
 import dash_ag_grid as dag
 import plotly.express as px
 import pandas as pd
+
+_dash_renderer._set_react_version('18.2.0')
 
 # Data
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
@@ -12,37 +15,53 @@ df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapmi
 app = Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 # Gridded layout
-app.layout = html.Div(id='main-container', children=[
-    # Header section
-    html.Div(className="header", children=[
-        html.H4(className="header-title", children=f'Boilerplate app on Dash v. {dash.__version__}'),
-    ]),
-    
-    # Map section
-    html.Div(className="map-container", children=[
-        dl.Map(dl.TileLayer(), center=[43,12], zoom=4, style={'height': '100%'})
-    ]),
-    
-    # Filters section
-    html.Div(className="filters-container", children=[
-        dcc.Dropdown(df.country.unique(), 'Italy', id='dropdown-selection')
-    ]),
-    
-    # Chart section
-    html.Div(className="chart-container", children=[
-        dcc.Graph(id='graph-content', style={"height":"100%"})
-    ]),
-    
-    html.Div(className="table-container", children=[
-        dag.AgGrid(
-            id="myGrid",
-            columnDefs= [{"field": x, } for x in df.columns],
-            rowData= df.to_dict('records'),
-            className="ag-theme-alpine",
-            columnSize="sizeToFit",
-            dashGridOptions={"pagination": True},
-            style={"height":"100%"}
-        ),
+app.layout = dmc.MantineProvider(children=[
+    html.Div(id='main-container', children=[
+        # Header section
+        html.Div(className="header", children=[
+            html.H4(className="header-title", children=f'Boilerplate app on Dash v. {dash.__version__}'),
+        ]),
+        
+        # Map section
+        html.Div(className="map-container", children=[
+            dl.Map(dl.TileLayer(), center=[43,12], zoom=4, style={'height': '100%'})
+        ]),
+        
+        # Filters section
+        html.Div(className="filters-container", children=[
+            # dcc.Dropdown(df.country.unique(), 'Italy', id='dropdown-selection')
+            dmc.Select(
+                radius=0,
+                size="sm", 
+                id="dropdown-selection",
+                value="Italy",
+                data=df.country.unique(),
+                # w=200,
+                # mb=10,
+            )
+        ]),
+        
+        # KPI section
+        html.Div(className="kpi-1", children=["KPI 1"]),
+        html.Div(className="kpi-2", children=[]),
+        
+        # Chart section
+        html.Div(className="chart-container", children=[
+            dcc.Graph(id='graph-content', style={"height":"100%"})
+        ]),
+        
+        # Table section
+        html.Div(className="table-container", children=[
+            dag.AgGrid(
+                id="myGrid",
+                columnDefs= [{"field": x, } for x in df.columns],
+                rowData= df.to_dict('records'),
+                className="ag-theme-alpine",
+                columnSize="sizeToFit",
+                dashGridOptions={"rowHeight": 30, "pagination": True},
+                style={"height":"100%"}
+            ),
+        ])
     ])
 ])
 
